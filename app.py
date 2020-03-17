167e4a51b09762c3d3aab77d11171cbe90cb6c87
JEH_MACMINI <JEH@JEH-MACMINI.local>
2020-03-17 13:40:23 +0800

    remoce python cv2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request, render_template, url_for
-from pycode.py_qrcode_reader import detect_qr_code
 import base64
 
 app = Flask(__name__, static_url_path='/static', static_folder="static")
@@ -12,10 +11,7 @@ def home():
 def detect():
     request_data = request.get_json()
     imagedata = request_data['imgBase64']
-    with open('qrcode.png', "wb") as fh:
-        fh.write(base64.decode(imagedata))
-    found, decode_data = detect_qr_code('qrcode.png', 'qrcode_output.png')
-    return decode_data
+    return jsonify({'encode_data':imagedata})
     
 
 if __name__ == '__main__':
