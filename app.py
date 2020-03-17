94e45b1b7f1aa67bfe0a6c2a35da558f055713b4
JEH_MACMINI <JEH@JEH-MACMINI.local>
2020-03-17 17:04:32 +0800

    testing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, render_template, url_for
 import base64
 from pycode.py_qrcode_reader import detect_qr_code
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__, static_url_path='/static', static_folder="static")
 
@@ -12,12 +14,16 @@ def home():
 def detect():
     try:
         request_data = request.get_json()
-        imagedata = request_data['imgBase64']
-        with open("rawimg.png", "wb") as fh:
-            fh.write(base64.decodebytes(imagedata))
+        print(request_data['imgBase64'])
+        imagedata = request_data['imgBase64'][22:]
+        print(imagedata)
+        # imagedata = base64.b64encode(imagedata)
+        im = Image.open(BytesIO(base64.b64decode(imagedata)))
+        im.save('rawimg.png', 'PNG')
         found, decode_data = detect_qr_code("rawimg.png","output.png")
     except Exception as e:
-        return jsonify({'found':False,'encode_data':e}), 500
+        print(e)
+        return jsonify({'found':False,'encode_data':"{}".format(e)}), 500
     
     return jsonify({'found':found,'encode_data':decode_data}), 200
     
