859e1a5bac02a3bb8e104ac3f90cd0a51d7dbecd
JEH_MACMINI <JEH@JEH-MACMINI.local>
2020-03-17 13:10:06 +0800

    add python server to analyze qrcode

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request, render_template, url_for
+import pycode.py_qrcode_reader import detect_qr_code
+import base64
 
 app = Flask(__name__, static_url_path='/static', static_folder="static")
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    request_data = request.get_json()
+    imagedata = request_data['imgBase64']
+    with open('qrcode.png', "wb") as fh:
+        fh.write(base64.decode(imagedata))
+    found, decode_data = detect_qr_code('qrcode.png', 'qrcode_output.png')
+    return decode_data
     
 
 if __name__ == '__main__':
